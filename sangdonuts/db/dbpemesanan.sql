57b81a3c82e5f0fed4b1be4957d79232096b122b
Yeni Q Aini <yeni29januari02@gmail.com>
2022-01-03 19:52:03 +0700

    Update dbpemesanan.sql

--- sangdonuts/db/dbpemesanan.sql
+++ sangdonuts/db/dbpemesanan.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 03 Jan 2022 pada 12.51
+-- Waktu pembuatan: 03 Jan 2022 pada 13.50
 -- Versi server: 10.4.17-MariaDB
 -- Versi PHP: 7.3.27
 
